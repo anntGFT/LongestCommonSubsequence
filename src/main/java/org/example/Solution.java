08b8cbb5bb9b6df6b8d220588e69e678321e89fe
annt <annt@gft.com>
2022-12-15 15:05:06 +0000

    Done with dp

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,28 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Solution {
+    public Solution(){
+
+    }
+
+    public int longestCommonSubsequence(String text1, String text2){
+
+        int m = text1.length(), n = text2.length();
+        int[][] aux = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+
+                    aux[i][j] = 1 + aux[i - 1][j - 1];
+                } else {
+
+                    aux[i][j] = Math.max(aux[i - 1][j], aux[i][j - 1]);
+                }
+            }
+        }
+
+        return aux[m][n];
     }
 }
--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -1,2 +1,53 @@
-package org.example;public class SolutionTest {
+package org.example;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+
+    static Solution solution;
+
+    String text1, text2;
+    int result;
+
+    @BeforeAll
+    static void setup(){
+
+        solution = new Solution();
+    }
+
+    @Test
+    public void noSubsequences(){
+
+        text1 = "abc";
+        text2 = "def";
+
+        result = 0;
+
+        assertEquals(result, solution.longestCommonSubsequence(text1, text2));
+    }
+
+    @Test
+    public void subsequenceOfLength3(){
+
+        text1 = "abcdef";
+        text2 = "acd";
+
+        result = 3;
+
+        assertEquals(result, solution.longestCommonSubsequence(text1, text2));
+    }
+
+    @Test
+    public void subsequenceOfLength5(){
+
+        text1 = "abcdefghijklmnopqrstuvwxyz";
+        text2 = "afity";
+
+        result = 5;
+
+        assertEquals(result, solution.longestCommonSubsequence(text1, text2));
+    }
 }
